bb0087715b009baa1f719ca74c2566399e3cbb10
Vanessa V. <redhotchiligal@yahoo.com>
2026-04-09 10:03:18 -0400

    Started but didnt finish

--- php/tests/LiftSystemTest.php
+++ php/tests/LiftSystemTest.php
@@ -11,17 +11,21 @@ use PHPUnit\Framework\TestCase;
 
 class LiftSystemTest extends TestCase
 {
-    // TODO: enable this test and finish writing it
-    public function testDoSomething(): void
+   
+    public function testFulfillRequest(): void
     {
-        $this->markTestSkipped('Enable this test and finish writing it');
+        // $this->markTestSkipped('Enable this test and finish writing it');
         /** @var Lift $liftA */
         $liftA = new Lift('A', 0);
 
         /** @var LiftSystem $lifts */
-        $lifts = new LiftSystem([0, 1], [$liftA], []);
+        $lifts = new LiftSystem([0, 1, 2, 3], [$liftA], []);
 
         $lifts->tick();
-        Approvals::verifyString((new LiftSystemPrinter())->printWithDoors($lifts));
+
+        $doors = (new LiftSystemPrinter())->printWithDoors($lifts);
+        // var_dump($doors);
+        // die();
+        Approvals::verifyString($doors);
     }
 }
